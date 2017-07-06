ccc122e7ded7417def6da5bcda4b03ca32e58e82
Serdar ÖZTUNA <serdaroztuna@gmail.com>
2017-07-06 18:03:10 +0300

    updates

--- src/app.js
+++ src/app.js
@@ -1,4 +1,10 @@
 import 'bootstrap';
+import $ from 'jquery';
+import 'owl.carousel';
 
 // Set moment locale
 moment.locale('tr');
+
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel();
+});
