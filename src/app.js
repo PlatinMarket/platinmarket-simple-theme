44d580862ba1026ddd35013362187f764f99db6c
Burak Doğan <me@burak-dogan.com>
2017-07-11 17:12:58 +0300

    Updates

--- src/app.js
+++ src/app.js
@@ -1,11 +1,6 @@
-// Load jQuery
-global.jQuery = global.$ = require('jquery');
-// Load Tether
-global.Tether = require('tether');
-// Load Bootstrap
-global.bt = require('bootstrap');
 // Load owl.carousel
 require('owl.carousel');
+
 // Load owl carousel mousemove
 const moveCarousel = require('./lib/move_carousel');
 
